0d7a4ca934711be03f352c2870f338b2c6af39e8
samadqureshi23 <162016414+samadqureshi23@users.noreply.github.com>
2024-04-20 18:59:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let c_value = {
+    PKR: 278.40,
+    UAE: 3.67,
+    INR: 3.32,
+    TAKA: 2.53,
+    SAR: 73.92,
+    OMR: 722.85,
+    CAD: 201.29,
+    CNY: 39.01,
+    JPY: 1.81,
+    AUD: 179.91,
+    NZD: 164.80,
+    USD: 1
+};
+let ans = await inquirer.prompt([{
+        type: "list",
+        name: "from",
+        message: "convertingg from",
+        choices: ['PKR', 'UAE', 'USD', 'INR', 'TAKA', 'SAR', 'OMR', 'CAD', 'CNY', 'JPY', 'AUD', 'NZD',]
+    },
+    {
+        type: "list",
+        name: "to",
+        message: "convertingg to",
+        choices: ['PKR', 'UAE', 'USD', 'INR', 'TAKA', 'SAR', 'OMR', 'CAD', 'CNY', 'JPY', 'AUD', 'NZD']
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "your amount to convert"
+    }
+]);
+console.log(c_value[ans.to] / c_value[ans.from] * ans.amount);
